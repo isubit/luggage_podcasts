071f6faf7cccd778096a4b9ea88ba005921b35b0
Nick Booher <njbooher@gmail.com>
2016-11-21 15:22:05 -0600

    SOY-18 Switch to library for iframe resizing

--- js/load_players.js
+++ js/load_players.js
@@ -1,1 +1,2 @@
 jQuery('audio.podlove-player-src').podlovewebplayer({staticEmbedPage: Drupal.settings.luggage_podcasts.library_dir + '/static.html'});
+jQuery('iframe.podlove-webplayer-frame').iFrameResize();
